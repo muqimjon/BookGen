77ac542454aca6ce7fbdc1fc3bc499efcb0f4abc
muqimjon <muqimjon.mr@gmail.com>
2025-06-07 14:44:42 +0500

    Update prod config and clean imports

--- bookgen.client/src/app/core/features/controls/controls.component.ts
+++ bookgen.client/src/app/core/features/controls/controls.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BookService } from '../../services/book.service';
