bfcd147782305650da435003a9b38c8b491e2d92
muqimjon <muqimjon.mr@gmail.com>
2025-06-08 16:21:11 +0500

    simple requires is done

--- BookGen.Server/Program.cs
+++ BookGen.Server/Program.cs
@@ -1,11 +1,15 @@
 ﻿using BookGen.Server.Services;
+using Microsoft.AspNetCore.HttpOverrides;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<BookService>();
 
-// Add services to the container.
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -22,9 +26,11 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 
 app.UseCors(policy => policy.AllowAnyOrigin()
                             .AllowAnyHeader()
-                            .AllowAnyMethod());
+                            .AllowAnyMethod()); 
 
-app.UseHttpsRedirection();
+app.UseForwardedHeaders();
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
--- bookgen.client/src/environments/environment.prod.ts
+++ bookgen.client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://13.60.42.203:8080'
+  apiUrl: 'https://book-api.idea-up.uz'
 };
